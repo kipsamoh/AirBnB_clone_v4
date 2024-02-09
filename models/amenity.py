59b13c655b4fb2c813b7aae62d1c3db892b4d698
kipsamoh <kipsamoh@gmail.com>
2024-02-09 03:58:48 -0800

    v4

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" _holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class Amenity(BaseModel, Base):
+    """_Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """_initializes Amenity"""
+        super().__init__(*args, **kwargs)
