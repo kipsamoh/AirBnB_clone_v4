28b086b795b381bc375fe185ff2dca002f2869e8
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-11 21:39:23 +0300

    10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts a Flash W_eb App_lication """
 from models import storage
 from models.state import State
 from models.city import City
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """ Remove the curr_ent SQLAlchemy Session """
     storage.close()
 
 
