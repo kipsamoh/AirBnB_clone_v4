7523fbe5dc4b310a19d832b93088d2e832ca6e38
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-11 18:58:06 +0300

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts a _Flash Web Application """
 from models import storage
 from models.state import State
 from os import environ
@@ -11,13 +11,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """ Remove the _current SQLAlchemy Session """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ displays a HTML page with a list of states """
+    """ _displays a HTML page with a list of states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
@@ -25,7 +25,7 @@ def states_list():
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
-    """ displays a HTML page with a list of cities by states """
+    """ displays a HTML page with a list of _cities by states """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
