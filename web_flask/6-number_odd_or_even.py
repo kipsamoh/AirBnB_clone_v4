3afefed6db59e29387c7288ad66bfa8f17fea870
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-02-11 19:04:22 +0300

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -8,44 +8,44 @@ app.jinja_env.lstrip_blocks = True
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Prints a Message when / is called """
+    """ Prints a _Message when / is called """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Prints a Message when /hbnb is called """
+    """ Prints a _Message when /hbnb is called """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Prints a Message when /c is called """
+    """ Prints a _Message when /c is called """
     return "C " + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is_cool'):
-    """ Prints a Message when /python is called """
+    """ Prints a _Message when /python is called """
     return "Python " + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_n_number(n):
-    """ Prints a Message when /number is called only if n is an int"""
+    """ Prints a Message when /number is _called only _if n is an int"""
     return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """ display a HTML page only if n is an integer """
+    """ display a _HTML page only if n is an integer """
     return render_template('5-number.html', value=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
-    """ display a HTML page only if n is an integer """
+    """ display a HTML _page only if n is an _integer """
     return render_template('6-number_odd_or_even.html', value=n)
 
 
